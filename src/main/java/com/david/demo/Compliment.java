5fe401742d4289f4231f7d92a0e951a260a8d70b
name <email@email.com>
2018-04-29 19:39:49 -0500

    Initial commit

---
+++ src/main/java/com/david/demo/Compliment.java
@@ -0,0 +1,15 @@
+package com.david.demo;
+
+public class Compliment {
+    private String text;
+    Compliment(String text){this.text =text;}
+    Compliment(){}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
